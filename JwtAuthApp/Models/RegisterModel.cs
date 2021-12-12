453b2ace116ca1eb91065345a7e119d1fb70f866
Manuel Valtingojer <manuel.va@gmx.net>
2021-12-12 12:52:41 +0100

    Create ApplicationDbContext.cs

---
+++ JwtAuthApp/Models/RegisterModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtAuthApp.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+    }
+}
