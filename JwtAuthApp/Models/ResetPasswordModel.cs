bf7b8f3d3959ecaded2c4b30fff9e69051d3c715
Manuel Valtingojer <manuel.va@gmx.net>
2021-12-13 18:48:18 +0100

    Web API Reset Password
    New Class ResetPasswordModel.cs

---
+++ JwtAuthApp/Models/ResetPasswordModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtAuthApp.Models
+{
+    public class ResetPasswordModel
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "NewPassword is required")]
+        public string NewPassword { get; set; }
+        
+        [Required(ErrorMessage = "ConfirmNewPassword is required")]
+        public string ConfirmNewPassword { get; set; }
+
+        public string Token { get; set; }
+    }
+}
